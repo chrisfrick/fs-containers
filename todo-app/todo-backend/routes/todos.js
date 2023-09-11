375128caa9abd91d9b0313c683778a4500e81729
chrisfrick <108237130+chrisfrick@users.noreply.github.com>
2023-09-11 16:01:23 -0400

    implement GET and PUT on todos/:id route Ex 12.7

--- todo-app/todo-backend/routes/todos.js
+++ todo-app/todo-backend/routes/todos.js
@@ -35,12 +35,22 @@ singleRouter.delete('/', async (req, res) => {
 
 /* GET todo. */
 singleRouter.get('/', async (req, res) => {
-  res.sendStatus(405); // Implement this
+  res.send(req.todo);
 });
 
 /* PUT todo. */
 singleRouter.put('/', async (req, res) => {
-  res.sendStatus(405); // Implement this
+  const id = req.todo
+  const todo = {
+    text: req.body.text,
+    done: req.body.done
+  }
+
+  console.log('todo: ', todo)
+
+  const updatedTodo = await Todo.findByIdAndUpdate(id, todo, { new: true })
+  console.log(updatedTodo)
+  res.send(updatedTodo) // Implement this
 });
 
 router.use('/:id', findByIdMiddleware, singleRouter)
